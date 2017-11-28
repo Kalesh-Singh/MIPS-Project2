520ceb088ead75232637c859b5ab53dcf806fb1d
Torment <kaleshsingh96@gmail.com>
2017-11-28 01:30:52 -0500

    Fixed code to deal with Too Large and NaN

--- main.asm
+++ main.asm
@@ -5,52 +5,52 @@
 	
 .text
 	Main:
-            # Get input from user
-            li $v0, 8
+            li $v0, 8                           # Get input from user
             la $a0, user_input
             li $a1, 1001
             syscall
             
-            # Get the address of where the input is stored
-            la $s0, user_input
+            la $s0, user_input                  # Get the address where the input string is stored
 
-            # Loop through the string to find the end index
             addi $a2, $zero, 0                  # Initialize the offsets to 0
 	        addi $a3, $zero, 0          	
     
             Loop4:
 		    # Loop through the string keeping a start ($a2) and end index ($a3) for every sub-string
-                add $t0, $a3, $s0               # Increment the address of the user input
+                add $t0, $a3, $s0               # Increment the address of the string
                 lb $t1, 0($t0)                  # Get the current character
                 beq $t1, 0, Convert             # If current char is endline char (0) --> Convert
                 beq $t1, 10, Convert            # If current char is newline char (10) --> Convert
-                bne $t1, 44, Increment         # If current char is NOT a coma --> Increment
+                bne $t1, 44, Increment          # If current char is NOT a coma --> Increment
 
                 Convert:
-                    jal subprogram_2                # Call subprogram 2
-                    jal subprogram_3                # Call subprogram 3
-                    j ResetStart                    # Jump to ResetStart
+                    jal subprogram_2            # Call subprogram 2
+                    jal subprogram_3            # Call subprogram 3
+                    j ResetStart                # Jump to ResetStart
 
                 NotANumber:
                     li $v0, 4                   # Print "NaN"
                     la $a0, nan
                     syscall
+                    j ResetStart
+
+                TooLarge:
+                    li $v0, 4                   # Print "too large"
+                    la $a0, too_large
+                    syscall
                 
                 ResetStart:
-                    add $t0, $a3, $s0               # Increment the address of the user input
-                    lb $t1, 0($t0)                  # Get the character at $a3
-                    beq $t1, 44, PrintComma         # If char is comma --> PrintComma ; else --> Exit
+                    add $t0, $a3, $s0           # Increment the address of the user input
+                    lb $a0, 0($t0)              # Get the character at $a3
+                    beq $a0, 44, PrintComma     # If char is comma --> PrintComma ; else --> Exit
                     j Exit
 
                     PrintComma:
-                        addi $a2, $a3, 1            # Set the start index to the end index plus 1
-
-                        addi $a0, $t1, 0            # Print the comma
-                        li $v0, 11
+                        addi $a2, $a3, 1        # Set the start index to the end index plus 1
+                        li $v0, 11              # Print the comma
                         syscall 
 
                 Increment:
-                    # Check if a3 is a /n or /0 and Exit---> IMPLEMENT THIS
                     addi $a3, $a3, 1            # Increment the End index
                     j Loop4
 
@@ -60,6 +60,8 @@
 			    syscall
 
 	subprogram_1:
+        # Converts the hex char in argument $a2 to its decimal value
+        # And returns the result in $v1
 
         add $t0, $zero, $a2                 # Copy the argument
         addi $t1, $zero, 87                 # Initialize the reference point to 87
@@ -68,33 +70,55 @@
         addi $t1, $zero, 55                 # Change the reference point to 55
         bgt $t0, 'F', NotANumber	        # If the char is greater than 'F' --> NotANumber
         bge $t0, 'A', Return1               # If char is within A to F --> Return 1
+        addi $t1, $zero, 48                 # Change the reference point to 48
         bgt $t0, '9', NotANumber	        # If the char is greater than '9' --> NotANumber
         blt $t0, '0', NotANumber	        # If the char is greater than '0' --> NotANumber
-        addi $t1, $zero, 48                 # Change the reference point to 48
         
         Return1:
             sub $v1, $t0, $t1               # Subtract the refernce from the character value  		
             jr $ra                          # and return the result in $v1
 	
     subprogram_2:
-        # Subprogram that converts a hexadecimal string to a decimal integer
-        # Arguments $a1 is the start of the string and $a2 is the end of the string
+        # Converts a hexadecimal substring to a decimal integer
+        # Argument $a1 is the start index of the substring and $a2 is the end of the substring
         # The result is returned via the stack
         
-        # Save return address of the caller function to stack
-        sw $ra, 0($sp) 
-        
-        # Copy the arguments
-        add $t2, $zero, $a2
+        sw $ra, 0($sp)                      # Save return address of the caller function to stack
+       
+        add $t2, $zero, $a2                 # Copy the arguments
         add $t3, $zero, $a3
 
-        # DECREMENT $t3 by 1
-        addi $t3, $t3, -1
-        
-        # Initialize the result to zero
-        addi $t4, $zero, 0
-        
-        Loop1:
+        Loop5:                              # Remove leading spaces and tabs from the substring
+            add $t5, $s0, $t2               # Go to the first char address in the substring
+            lb $t0, 0($t5)                  # Get the char at that address
+            
+            beq $t0, 32, IncrementStart     # If current char is a space --> IncrementStart
+            beq $t0, 9, IncrementStart      # If current char us a tab --> IncrementStart
+            beq $t2, $t3, NotANumber        # If substring is all spaces or tabs --> NaN            
+
+            addi $t3, $t3, -1               # Decrement the end index by 1
+            j Loop6                         # Go to Loop6
+                        
+            IncrementStart:
+                addi $t2, $t2, 1            # Increment the start index
+                j Loop5
+
+        Loop6:                              # Remove lagging spaces and tabs from the substring
+            add $t5, $s0, $t3               # Go to the last char address in the substring
+            lb $t0, 0($t5)                  # Get the char at that address
+
+            beq $t0, 32, DecrementEnd       # If current char is a space --> DecrementEnd
+            beq $t0, 9, DecrementEnd        # If current char us a tab --> DecrementEnd
+
+            sub $s1, $t3, $t2               # Get the len of the substring
+            addi $t4, $zero, 0              # Initialize the result to zero
+            j Loop1
+
+            DecrementEnd:
+                addi $t3, $t3, -1           # Decrement the end index
+                j Loop6
+
+        Loop1:                              # Convert the hex string to its decimal value
             add $t5, $s0, $t2               # Go to the next character address in the string
             lb $a2, 0($t5)                  # Get the character at that address
             jal subprogram_1         	    # Get the decimal value of the character
@@ -105,7 +129,8 @@
             j Loop1
         
         Return2:
-            lw $ra, 0($sp)			        # Get the old return value
+            bgt $s1, 7, TooLarge          # If the length of the string greater than 8 --> TooLarge
+            lw $ra, 0($sp)			        # Get the old return address
             sw $t4, 0($sp)                  # Store the result on the stack
             jr $ra    
     
@@ -119,32 +144,26 @@
         addi $t1, $zero, 10                 # Set the divisor to 10
         addi $t4, $zero, 0                  # Initialize a counter to 0
 
-        Loop2:
+        Loop2:                              # Get the digits
             divu $t0, $t1                   # $t0 / $t1 --> Remainder in HIGH, Quotient in LOW
             mflo $t0                        # Set the new Dividend to be the Quotient
             mfhi $t2                        # Get the Remainder (digit)
-            addi $t2, $t2, 48               # Convert the digit to its ASCII character code
 
-            # Store the address of the stack pointer in $t3 and add the counter to it
-            la $t3, ($sp)
-            add $t3, $t3, $t4       
-
-            sb $t2, 0($t3)                  # Store the least siginificant byte of the remainder on the stack at the address of $t3
-            # If the Dividend is 0 --> Exit the loop to Loop3
-            beq $t0, $zero, Loop3
-
+            la $t3, ($sp)                   # Store the address of the stack pointer in $t3
+            add $t3, $t3, $t4               # Add the offset to the address
+            sb $t2, 0($t3)                  # Store the remainder on the stack at the address of $t3
+            beq $t0, $zero, Loop3           # If the Dividend is 0 --> Exit the loop to Loop3
             addi $t4, $t4, 1                # Increment the counter
 
             j Loop2
 
-        Loop3:
-            # Print the digits in the reverse order
+        Loop3:                              # Print the digits
             lb $a0, 0($t3)
-            li $v0, 11
+            li $v0, 1
             syscall
             
             beq $t3, $sp, Return3           # If the address in $t3 == address in $sp --> Return3
-            addi $t3, $t3, -1               # Decrement the addreess in $t3
+            addi $t3, $t3, -1               # Decrement the address in $t3
             j Loop3        
         Return3:
             jr $ra	
